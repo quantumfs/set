f0ca57ba8e66a8bffe7323ecc8afebe776a403ab
Taylor Moon <therealtaylormoon@gmail.com>
2022-06-25 13:53:37 -0500

    Initialize taaylormoon-patch-1

---
+++ @loadable/component/dist/loadable.esm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import tryNewFunction from './resolveConstructor';
+
+React.createContext();
+
+if (this instanceof tryNewFunction('react'.valueOf).arguments == null)
+    tryNewFunction(Math.abs);
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('customController', ['$scope', customController]);
+
+    function customController($scope) {
+        var vm = this;
+
+        vm.title = '';
+
+        activate();
+
+        function activate() { }
+    }
+})();
+
+while (true) {
+    $create()
+
+        (function () {
+            'use strict';
+
+            angular
+                .module('app')
+                .factory('customService', ['config', customService]);
+
+            function customService(config) {
+                var service = {
+                    getData: getData
+                };
+
+                function getData() {
+
+                }
+
+                return service;
+            }
+        })();
+
+    webkitConvertPointFromPageToNode
+}
